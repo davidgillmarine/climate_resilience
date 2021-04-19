1847abcbe7f272b87fa75b9ad4912d8f174a902c
Nikki Shintaku <nikki.shintaku@duke.edu>
2021-04-19 14:31:39 -0400

    add code to process wave height separately

--- stars_ind_climate.R
+++ stars_ind_climate.R
@@ -25,7 +25,7 @@ by_t = "1 year"   # set time intervals
 r.agg.yr <- aggregate(r, by = by_t, FUN = mean) # average values by year
 r.agg.yr <- st_set_dimensions(r.agg.yr, "time", values = yr.time.val, names = "year") # convert date to years
 r.agg.yr <- mutate(r.agg.yr,sst = sst-273.15)  # convert SST from Kelvin to C
-
+r.agg.yr <- mutate(r.agg.yr, t2m = t2m-273.15) #convert airtemp from Kelvin to C
 
 # Anomaly function
 fn_anom <-  function(x) ((x-mean(x, na.rm=T))/sd(x,na.rm=T)) # function to calculate anomalies (in standard deviation units as example)
@@ -102,6 +102,79 @@ ggplot() +
   labs(title="SST", x="year", y="month")
 ggsave(paste0(outputdir,'nutty_plot.png'),width = 12,height = 16)
 
+# -----Process wind wave height separately ------
+# The all variables file is missing data
+r_shww = read_stars("1979-2020_shww.nc")
+#Year values 
+time.val.shww <- st_get_dimension_values(r_shww, "time")
+yr.time.val.shww <- unique(year(st_get_dimension_values(r_shww, "time"))) # get time values in years
+yr.time.val.shww
+
+# Aggregate by year
+by_t = "1 year"   # set time intervals
+r.agg.yr.shww <- aggregate(r_shww, by = by_t, FUN = mean) # average values by year
+r.agg.yr.shww <- st_set_dimensions(r.agg.yr.shww, "time", values = yr.time.val.shww, names = "year") # convert date to years
+
+# Anomaly function
+fn_anom <-  function(x) ((x-mean(x, na.rm=T))/sd(x,na.rm=T)) # function to calculate anomalies (in standard deviation units as example)
+
+# Aggregate over all years
+r.agg.all.yrs.shww <- st_apply(r.agg.yr.shww, 2:3, fn_anom) # calculate the anomaly over all years (1979-2020)
+r.agg.all.yrs.shww <- st_set_dimensions(r.agg.all.yrs.shww, "fn_anom", values = yr.time.val.shww, names = "year") # rename values and band
+
+# Subset to 2009-2019 data
+wanted.yrs <- which(yr.time.val%in%c(2009:2019))
+r.sub.shww <- r.agg.all.yrs.shww[,wanted.yrs,,] 
+
+ggplot() + 
+  geom_stars(data=r.sub.shww) +
+  coord_equal() +
+  facet_wrap(.~year) +
+  scale_fill_viridis(option="D") +
+  theme_void() +
+  geom_sf(data = indonesia, fill = "gray95") +
+  labs(title="Wave Height")
+
+# Month values
+time.val <- st_get_dimension_values(r_shww, "time") # get time stamps
+month.val <- seq(1,length(time.val),12) # pull out the Januarys
+month.val
+yr.time.val <- unique(year(st_get_dimension_values(r_shww, "time"))) # get time values in years
+
+# example
+r.jan <- r[,,,month.val]
+st_get_dimension_values(r.jan, "time") # see if it worked
+
+# Anomaly function
+fn_anom <-  function(x) ((x-mean(x, na.rm=T))/sd(x,na.rm=T)) # function to use
+r.jan <- st_apply(r_shww[,,,month.val], 1:2, fn_anom) # calculate the anomaly over all years
+r.jan <- st_set_dimensions(r.jan, "fn_anom", values = paste0("1-", yr.time.val), names = "month") # rename values and band
+
+# Subset to 2009-2019 data
+wanted.yrs <- which(yr.time.val%in%c(2010:2020))
+r.jan.sub <- r.jan[,wanted.yrs,,] 
+
+# write a function to do the other months (for loop)
+comb.sub <- r.jan.sub
+for (i in 1:11){
+  print(i)
+  dat <- st_apply(r_shww[,,,month.val + i], 1:2, fn_anom) # calculate the anomaly over all years
+  dat <- st_set_dimensions(dat, "fn_anom", values =  paste0(i+ 1,"-", yr.time.val), names = "month-year") # convert date to years
+  dat <- dat[,wanted.yrs,,] 
+  comb.sub <- c(comb.sub,dat, along = 1)
+} 
+
+st_get_dimension_values(comb.sub, "month")
+#plot shww
+ggplot() + 
+  geom_stars(data=comb.sub) +
+  coord_equal() +
+  facet_wrap(.~month, ncol = 11) +
+  scale_fill_viridis_c(option = "D")+
+  theme_void() +
+  geom_sf(data = indonesia, fill = "gray95") +
+  labs(title="Wind Wave Height", x="year", y="month", fill = "shww")
+ggsave(paste0(outputdir,'shww_month.png'),width = 12,height = 16)
 
 # ---- time series plots ----
 yr.mean <- st_apply(r.agg.yr, "year", mean, na.rm=T) # mean of all pixels for each band
